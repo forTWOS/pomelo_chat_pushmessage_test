731509b7e0caabe18da05791b4d91e5ee89c22e3
Linqy <linqiyo@126.com>
2018-04-16 20:29:29 +0800

    for init

---
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -0,0 +1,61 @@
+module.exports = function(app) {
+  return new Handler(app);
+};
+
+var Handler = function(app) {
+  this.app = app;
+  this.cn  = this.app.get('channelService').getChannel('1',true);
+};
+
+/**
+ * New client entry.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.entry = function(msg, session, next) {
+    var uid = session.id;
+    if (!this.app.get('sessionService').getByUid(uid)) {
+        session.bind(uid);
+        session.pushAll();
+        this.cn.add(uid, this.app.getServerId());
+    }
+    this.cn.pushMessage('test',{"msg":"di"}, function() {
+        console.log(arguments);
+    });
+  next(null, {code: 200, msg: 'game server is ok.'});
+};
+
+/**
+ * Publish route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.publish = function(msg, session, next) {
+	var result = {
+		topic: 'publish',
+		payload: JSON.stringify({code: 200, msg: 'publish message is ok.'})
+	};
+  next(null, result);
+};
+
+/**
+ * Subscribe route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.subscribe = function(msg, session, next) {
+	var result = {
+		topic: 'subscribe',
+		payload: JSON.stringify({code: 200, msg: 'subscribe message is ok.'})
+	};
+  next(null, result);
+};
